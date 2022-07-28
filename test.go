70798c99bcfa29db7f465b308b3e4e996fe317d9
Matthew Sevey <mjsevey@gmail.com>
2022-07-28 15:38:14 -0400

    Init git

---
+++ test.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// weight, height, tire
+	if !weight {
+		return error because !weight
+	}
+
+	if !height {
+		return error because !height
+	}
+
+	if !tire {
+		return error b/e !tire
+	}
+
+	// all condidions are met
+	return result
+}
